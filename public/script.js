352595363b4ca36e6a32bb6be2f4e5131a7de605
ssbor <karlovskabarbora@email.cz>
2025-11-06 16:49:06 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -4,17 +4,15 @@ const regionFilter = document.getElementById('regionFilter');
 function filterJobs(allJobs) {
     const selectedRegion = regionFilter.value;
 
-    const filteredJobs = allJobs.filter(job => {
-        if (selectedRegion === 'zahranici_bor') {
-            return job.area === 'zahranici_bor';
-        } else {
-            return !selectedRegion || job.region === selectedRegion;
-        }
-    });
+   const filteredJobs = allJobs.filter(job => {
+    const selectedRegion = regionFilter.value;
 
-    return filteredJobs;
-}
-
+    if (selectedRegion === 'zahranici_bor') {
+        return job.area === 'zahranici_bor';
+    } else {
+        return !selectedRegion || job.region === selectedRegion;
+    }
+});
 // Příklad použití:
 // const jobs = [...]; // pole všech nabídek
 // const visibleJobs = filterJobs(jobs);
